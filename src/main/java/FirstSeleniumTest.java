11f1d4d65216304be0a8e190720eb6ae89a6f637
maksudur-rahman-maruf <maruf.bsnm@gmail.com>
2021-11-05 18:28:36 +0600

    added web manager driver

--- src/main/java/FirstSeleniumTest.java
+++ src/main/java/FirstSeleniumTest.java
@@ -1,13 +1,16 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class FirstSeleniumTest {
     public static void main(String[] args) throws InterruptedException {
 
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         String searchElement = "[name=" + '"' + "q" + '"' + "]";
 
